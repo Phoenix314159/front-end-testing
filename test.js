d561f20eaf28729c9808a1275e65f9b1c7367915
James Langford <derivative7117@live.com>
2017-09-06 02:48:55 -0600

    initial commit

---
+++ test.js
@@ -0,0 +1,9 @@
+// const chai = require('chai'),
+var assert = chai.assert;
+
+describe('array', () => {
+    var arr = [];
+    it('Should start empty', () => {
+        assert.equal(arr.length, 0);
+    })
+})
